2ed579f58b335d28beae87c23c6c2475b03e83f1
LAPTOP-9618V2DO\POINT-I <agratos318@naver.com>
2024-05-02 13:44:14 +0900

    no message

--- todo-app/server/routes/index.js
+++ todo-app/server/routes/index.js
@@ -4,5 +4,8 @@ const router = exporess.Router();
 const todoApi = require('./todos.api');
 
 router.use('/todos', todoApi);
+router.get('/test', (req, res) => {
+	res.send('Server Success');
+});
 
 module.exports = router;
