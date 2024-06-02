50e0df2e9edbebdf4f95914aa438afbf03896bfe
DESKTOP-ODP6G2Q\agrat <agratos318@naver.com>
2024-06-02 21:29:55 +0900

    no message

--- H&M/server/controllers/auth.controller.js
+++ H&M/server/controllers/auth.controller.js
@@ -15,6 +15,8 @@ authController.loginWithEmail = async (req, res) => {
 				// token
 				const token = await user.generateToken();
 				return res.status(200).json({ status: 'success', user, token });
+			}else{
+				throw new Error('비밀번호와 이메일이 유효하지 않습니다.');
 			}
 		} else {
 			throw new Error('비밀번호와 이메일이 유효하지 않습니다.');
